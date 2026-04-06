fd00e5392ba1d709ce7d10db50ddedc204bbf847
Olalekandadzeke100 <wahabolamilekan92@gmail.com>
2026-04-05 19:10:49 -0500

    api check

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 // Base URL for Django backend
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || "http://127.0.0.1:8000";
+  process.env.NEXT_PUBLIC_API_BASE;
 
 /**
  * Fetch product list
