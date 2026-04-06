f62ca8528afa130c2a3dcb3e067352af15ab1cae
Olalekandadzeke100 <wahabolamilekan92@gmail.com>
2026-04-05 20:57:53 -0500

    api update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -56,6 +56,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "http://localhost:3005",
+    "https://nellyluxfrontend-production.up.railway.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
